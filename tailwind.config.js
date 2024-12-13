296c9c16b5ea583735d41abd0edaef7f9fe5e1a9
Tamim khan <mdtamimkhan842@gmail.com>
2024-12-14 02:48:18 +0600

    added varticalproject

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontfamily: {
+        poppins: ['Poppins', 'sans- serif'],
+      },
+      colors: {
+        'bg-color': '#091420',
+        'bg-dark-color': '#0c1a28',
+        'primary-color': '#4fe98c',
+      },
+      width: {
+        'custom-image-width': '200px',
+      },
+    },
+
+
+  },
+  plugins: [],
+}
+
